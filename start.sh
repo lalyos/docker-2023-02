f6b320e2f8e3c742d6d38d184f0c4dcf5ccacba4
lalyos <lalyos@yahoo.com>
2023-02-22 14:42:05 +0100

    COLOR/BODY env added

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-echo ${TITLE:-Welcome} > /var/www/html/index.html
+cat > /var/www/html/index.html <<EOF
+<html>
+<body bgcolor="${COLOR:-gray}">
+  <h1>${TITLE:-Welcome}</h1>
+  ${BODY:- please use COLOR/TITLE/BODY env vars}
+</body>
+</html>
+EOF
 
 nginx -g "daemon off;"
