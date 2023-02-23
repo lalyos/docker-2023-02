e809b69a0f5482b5a83a94baf10f5eac555a6799
lalyos <lalyos@yahoo.com>
2023-02-23 12:03:00 +0100

    add init sql

---
+++ sql/init.sql
@@ -0,0 +1,8 @@
+use mysql;
+
+create table VIP (name varchar(20), age int, money int);
+
+insert into VIP values ('vladimir', 67, 30);
+insert into VIP values ('victor', 63, 0);
+insert into VIP values ('lorinc', 63, 50);
+insert into VIP values ('gyuri',60  ,20);
